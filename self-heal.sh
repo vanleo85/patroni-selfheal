e17c25a6d07eb201461143729c5fa66c582ddfe4
Leonid Vanin <vanl@automacon.ru>
2023-12-05 10:31:51 +0300

    +

--- self-heal.sh
+++ self-heal.sh
@@ -8,7 +8,8 @@ trap exit TERM INT
 while true; do
     echo "$(date +'%d.%m.%Y %H:%M:%S') - Checking Patroni health..."
 
-    if curl -s localhost:8008 | grep -q '"state": "start failed"' || curl -s localhost:8008 | grep -q '"state": "stopped"'; then
+    # if curl -s localhost:8008 | grep -q '"state": "start failed"' || curl -s localhost:8008 | grep -q '"state": "stopped"'; then
+    if curl -s localhost:8008 | grep -q '"state": "start failed"'; then
         echo "$(date +'%d.%m.%Y %H:%M:%S') - Patroni start failed :("
         echo "$(date +'%d.%m.%Y %H:%M:%S') - Deleting pod..."
         curl -k -X DELETE "https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_PORT_443_TCP_PORT}/api/v1/namespaces/${POD_NAMESPACE}/pods/${POD_NAME}" --data '{"gracePeriodSeconds":0,"propagationPolicy":"Background"}' -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -H "Accept: application/json" -H "Content-Type: application/json"
